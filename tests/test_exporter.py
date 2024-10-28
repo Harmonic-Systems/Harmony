ed82c0c5d3750b244bda9624e8fb8f65486adaa5
Anas Poe <anaspoe23@carox.tech>
2024-10-28 13:15:15 +0200

    exporting: do not include api keys in the main .py/ipynb

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -11,17 +11,21 @@ from harmony.models import HarmonyFlow
 from .exporting.flow_helpers import get_flow
 
 
-def test_export_load_from_file(harmony_flow: HarmonyFlow) -> None:
+def test_export_load_from_file(
+    tmp_path: Path, harmony_flow: HarmonyFlow
+) -> None:
     """Test exporting and loading from file.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.harmony")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.harmony"
     exporter.export(str(output_file))
     assert output_file.exists()
     HarmonyExporter.load(output_file)
@@ -34,34 +38,42 @@ def test_exporter_load_invalid_path() -> None:
         HarmonyExporter.load(Path("non_existent_file"))
 
 
-def test_exporter_use_directory(harmony_flow: HarmonyFlow) -> None:
+def test_exporter_use_directory(
+    tmp_path: Path, harmony_flow: HarmonyFlow
+) -> None:
     """Test exporter use directory.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_dir = Path(f"{uuid.uuid4().hex}.harmony")
+    output_dir = tmp_path / f"{uuid.uuid4().hex}.harmony"
     output_dir.mkdir()
     with pytest.raises(IsADirectoryError):
         exporter.export(output_dir)
     output_dir.rmdir()
 
 
-def test_exporter_file_exists(harmony_flow: HarmonyFlow) -> None:
+def test_exporter_file_exists(
+    tmp_path: Path, harmony_flow: HarmonyFlow
+) -> None:
     """Test exporter file exists.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.harmony")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.harmony"
     output_file.touch()
     with pytest.raises(FileExistsError):
         exporter.export(output_file)
@@ -69,94 +81,112 @@ def test_exporter_file_exists(harmony_flow: HarmonyFlow) -> None:
     output_file.unlink(missing_ok=True)
 
 
-def test_exporter_force(harmony_flow: HarmonyFlow) -> None:
+def test_exporter_force(tmp_path: Path, harmony_flow: HarmonyFlow) -> None:
     """Test exporter force.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.harmony")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.harmony"
     output_file.touch()
     exporter.export(output_file, force=True)
     output_file.unlink(missing_ok=True)
 
 
-def test_export_to_py(harmony_flow: HarmonyFlow) -> None:
+def test_export_to_py(tmp_path: Path, harmony_flow: HarmonyFlow) -> None:
     """Test exporting to Python.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.py")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.py"
     exporter.export(output_file)
     assert output_file.exists()
     output_file.unlink(missing_ok=True)
 
 
-def test_export_to_ipynb(harmony_flow: HarmonyFlow) -> None:
+def test_export_to_ipynb(tmp_path: Path, harmony_flow: HarmonyFlow) -> None:
     """Test exporting to Jupyter Notebook.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.ipynb")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.ipynb"
     exporter.export(output_file)
     assert output_file.exists()
     output_file.unlink(missing_ok=True)
 
 
-def test_export_to_harmony(harmony_flow: HarmonyFlow) -> None:
+def test_export_to_harmony(tmp_path: Path, harmony_flow: HarmonyFlow) -> None:
     """Test exporting to Harmony file.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.harmony")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.harmony"
     exporter.export(output_file)
     assert output_file.exists()
     output_file.unlink(missing_ok=True)
 
 
-def test_export_to_invalid_extension(harmony_flow: HarmonyFlow) -> None:
+def test_export_to_invalid_extension(
+    tmp_path: Path, harmony_flow: HarmonyFlow
+) -> None:
     """Test exporting to invalid extension.
 
     Parameters
     ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
     harmony_flow : HarmonyFlow
         A HarmonyFlow instance.
     """
     harmony = Harmony(flow=harmony_flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.invalid")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.invalid"
     with pytest.raises(ValueError):
         exporter.export(output_file)
 
 
-def test_export_complex_flow() -> None:
-    """Test exporting invalid flow."""
+def test_export_complex_flow(tmp_path: Path) -> None:
+    """Test exporting invalid flow.
+
+    Parameters
+    ----------
+    tmp_path : Path
+        A pytest fixture to provide a temporary directory.
+    """
     flow = get_flow()
     harmony = Harmony(flow=flow)
     exporter = HarmonyExporter(harmony)
-    output_file = Path(f"{uuid.uuid4().hex}.py")
+    output_file = tmp_path / f"{uuid.uuid4().hex}.py"
     exporter.export(output_file)
     assert output_file.exists()
     output_file.unlink()
-    skill_file = Path("skill_name.py")
+    skill_file = tmp_path / "skill_name.py"
     assert skill_file.exists()
     skill_file.unlink(missing_ok=True)
